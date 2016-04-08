47a7640d4218e2d67177ea1bd3088c2930a567b0
Ravi Rani <ravirani@gmail.com>
2016-04-08 01:37:14 -0700

    Remove boilerplate sample components

--- client/routes/Route.js
+++ client/routes/Route.js
@@ -1,18 +1,12 @@
 import React from 'react';
-import { IndexRoute, Route, Redirect } from 'react-router';
+import { Route, Redirect } from 'react-router';
 
 import AppComponent from '../components/App/AppComponent';
-import FeatureComponent from '../components/Feature/FeatureComponent';
-import LoginComponent from '../components/Login/LoginComponent';
 import MessageLogComponent from '../components/MessageLog/MessageLogComponent';
-import SignupComponent from '../components/Signup/SignupComponent';
 import SmartAnswerComponent from '../components/SmartAnswer/SmartAnswerComponent';
 
 export default (
   <Route path='/' component={AppComponent}>
-    <IndexRoute component={FeatureComponent} />
-    <Route path='/signup' component={SignupComponent} />
-    <Route path='/login' component={LoginComponent} />
     <Route path='/message-logs' component={MessageLogComponent} />
     <Route path='/smart-answers' component={SmartAnswerComponent} />
     <Redirect from='*' to='/' />
