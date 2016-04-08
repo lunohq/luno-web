fcd172fee8c846936beb05de7bb68e65ad026e1a
Ravi Rani <ravirani@gmail.com>
2016-04-08 01:59:31 -0700

    Add redux and redux-form

--- client/index.js
+++ client/index.js
@@ -1,5 +1,8 @@
 import { browserHistory } from 'react-router';
+import { createStore, combineReducers } from 'redux';
+import { reducer as formReducer } from 'redux-form';
 import injectTapEventPlugin from 'react-tap-event-plugin';
+import { Provider } from 'react-redux';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { RelayRouter } from 'react-router-relay';
@@ -7,11 +10,18 @@ import { RelayRouter } from 'react-router-relay';
 import '../node_modules/react-mdl/extra/material.js';
 import Route from './routes/Route';
 
+const reducers = {
+  form: formReducer,
+};
+const reducer = combineReducers(reducers);
+const store = createStore(reducer);
 const rootNode = document.createElement('div');
 document.body.appendChild(rootNode);
 
 ReactDOM.render(
-  <RelayRouter history={browserHistory} routes={Route} />,
+  <Provider store={store}>
+    <RelayRouter history={browserHistory} routes={Route} />,
+  </Provider>,
   rootNode
 );
 
