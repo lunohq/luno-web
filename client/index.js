2b8ba3b811cd711adf38cea18ce7a98cb06da139
Ravi Rani <ravirani@gmail.com>
2016-04-08 11:44:53 -0700

    Redux no likey relay

--- client/index.js
+++ client/index.js
@@ -1,8 +1,5 @@
 import { browserHistory } from 'react-router';
-import { createStore, combineReducers } from 'redux';
-import { reducer as formReducer } from 'redux-form';
 import injectTapEventPlugin from 'react-tap-event-plugin';
-import { Provider } from 'react-redux';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { RelayRouter } from 'react-router-relay';
@@ -10,18 +7,11 @@ import { RelayRouter } from 'react-router-relay';
 import '../node_modules/react-mdl/extra/material.js';
 import Route from './routes/Route';
 
-const reducers = {
-  form: formReducer,
-};
-const reducer = combineReducers(reducers);
-const store = createStore(reducer);
 const rootNode = document.createElement('div');
 document.body.appendChild(rootNode);
 
 ReactDOM.render(
-  <Provider store={store}>
-    <RelayRouter history={browserHistory} routes={Route} />,
-  </Provider>,
+  <RelayRouter history={browserHistory} routes={Route} />,
   rootNode
 );
 
