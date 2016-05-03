eeec6bbcf803ad41843bdf68e9704bc3f856f1c9
Michael Hahn <mwhahn@gmail.com>
2016-05-03 15:51:21 -0700

    Fix background color issue

--- client/components/AnonymousLanding/Component.js
+++ client/components/AnonymousLanding/Component.js
@@ -7,14 +7,14 @@ import './style.scss'
 const AnonymousLanding = () => (
   <DocumentTitle title='Login to your Luno'>
     <div className='landing'>
-      <div className='navbar navbar-default navbar-fixed-top'>
-        <div className='container'>
+      <div className='container'>
+        <div className="header row">
           <div className='col-xs-6'>
-            <a className='navbar-brand' href='#'>luno</a>
+            <a className='brand' href='#'>luno</a>
           </div>
         </div>
       </div>
-      <div classname='container'>
+      <div className='content container'>
         <div className='row center-xs'>
           <div className='col-xs-12'>
             <h1>Log in to your Slack team</h1>
