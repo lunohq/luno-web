acca2a153ed6c3cd452d24cd3250395a03b36f3d
Michael Hahn <mwhahn@gmail.com>
2016-05-09 13:48:19 -0700

    Only upload the explanation if we request verbose results

--- server/handlers.js
+++ server/handlers.js
@@ -11,8 +11,9 @@ export function handleSlashCommand(bot, message) {
     text = parts.slice(1).join(' ')
   }
 
-  if (command === 'explain') {
-    return handleExplain({ bot, message, text })
+  if (command.startsWith('explain')) {
+    const verbose = command.endsWith('[v]')
+    return handleExplain({ bot, message, text, verbose })
   } else {
     return handleDefault({ bot, message })
   }
@@ -22,31 +23,33 @@ function formatResult({ _source: source, _score: score }) {
   return `*title:* ${source.title}, *score*: ${score}`
 }
 
-function explainResults({ bot, message, query, result }) {
+function explainResults({ bot, message, query, result, verbose }) {
   return new Promise(async (resolve) => {
     const parts = [`Found the following results for: *${query}*`]
     const formattedResults = result.hits.hits.map(hit => formatResult(hit))
     parts.push(...formattedResults)
-
-    let upload
-    try {
-      upload = await uploadResult({ bot, message, query, result })
-    } catch (err) {
-      logger.error('Error uploading result', metadata({ bot, message, err, query, result }))
-      return reject()
-    }
-
     bot.replyPrivate(message, parts.join('\n'))
-    bot.replyPrivateDelayed(message, `Full explanation: ${upload.permalink}`, (err) => {
-      if (err) {
-        logger.error('Error sending delayed message', metadata({ err, message }))
+
+    if (verbose) {
+      let upload
+      try {
+        upload = await uploadResult({ bot, message, query, result })
+      } catch (err) {
+        logger.error('Error uploading result', metadata({ bot, message, err, query, result }))
+        return reject()
       }
-    })
+
+      bot.replyPrivateDelayed(message, `Full explanation: ${upload.permalink}`, (err) => {
+        if (err) {
+          logger.error('Error sending delayed message', metadata({ err, message }))
+        }
+      })
+    }
     return resolve()
   })
 }
 
-export async function handleExplain({ bot, message, text }) {
+export async function handleExplain({ bot, message, text, verbose }) {
   let result
   try {
     result = await search(bot.config.luno.botId, text)
@@ -54,7 +57,7 @@ export async function handleExplain({ bot, message, text }) {
     logger.error('Error executing search', metadata({ err, bot, message }))
   }
   try {
-    await explainResults({ bot, message, query: text, result })
+    await explainResults({ bot, message, query: text, result, verbose })
   } catch (err) {
     logger.error('Error explaining results', metadata({ err, bot, message, result }))
   }
