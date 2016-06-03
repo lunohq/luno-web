ecde001a86478eb7a20338c25905184d11c4736d
awo <allen.wo@gmail.com>
2016-06-03 00:22:34 -0700

    moved reposition into the dialogHeightFix object; added trailing commas to CSS

--- client/components/Dialog.js
+++ client/components/Dialog.js
@@ -6,24 +6,24 @@ const dialogHeightFix = {
     display: 'flex',
     alignItems: 'center',
     justifyContent: 'center',
-    paddingTop: 0
+    paddingTop: 0,
   },
   contentStyle: {
     position: 'relative',
     width: '75vw',
     maxWidth: 768,
-    transform: 'none'
+    transform: 'none',
   },
   bodyStyle: {
-    paddingBottom: 0
-  }
+    paddingBottom: 0,
+  },
+  repositionOnUpdate: false,
 }
 
 const Dialog = (props) => (
   <MaterialDialog
     {...props}
     {...dialogHeightFix}
-    repositionOnUpdate={false}
   />
 )
 
