d3f8326cb52d2039fb9e6af719f158c299dd7460
Michael Hahn <mwhahn@gmail.com>
2016-06-06 15:36:46 -0700

    Add helpers to run management scripts

---
+++ server/management/scripts/executor.js
@@ -0,0 +1,32 @@
+/**
+ * Executes and runs the specified script
+ *
+ */
+
+import logger from '../../logger'
+
+async function execute({ script }) {
+  const started = new Date()
+  logger.info(`Executing script: ${script}...`)
+
+  let run
+  try {
+    run = require(`./${script}`).default
+  } catch (err) {
+    logger.error('Error loading script', { err, script })
+    throw err
+  }
+  try {
+    await run()
+  } catch (err) {
+    logger.error('Error running script', { err, script })
+    throw err
+  }
+  const ended = new Date()
+  logger.info(`...executed script: ${(ended - started) / 1000} seconds`)
+}
+
+execute({ script: process.argv[2] }).catch((err) => {
+  logger.error(err)
+  os.exit(1)
+})
