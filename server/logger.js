24f31a6f8571ef4cf1abd159f1b87c33437df188
Michael Hahn <mwhahn@gmail.com>
2016-06-08 16:39:49 -0700

    Add rewriters

--- server/logger.js
+++ server/logger.js
@@ -3,6 +3,47 @@ import RavenWinston from 'raven-winston'
 
 import config from './config/environment'
 
+function rewriteError(level, msg, meta) {
+  const output = {}
+  if (meta instanceof Error) {
+    output.err = {
+      stack: meta.stack,
+      message: meta.message,
+      extra: meta.extra,
+    }
+    return output
+  }
+  Object.assign(output, meta)
+  if (meta.err) {
+    output.err = {
+      stack: meta.err.stack,
+      message: meta.err.message,
+      extra: meta.err.extra,
+    }
+  }
+  return output
+}
+
+function scrub(level, msg, meta) {
+  const protectedKeys = ['token', 'accessToken']
+
+  function _scrub(obj) {
+    for (const key of Object.keys(obj)) {
+      if (protectedKeys.includes(key)) {
+        obj[key] = '********'
+        continue
+      }
+      value = obj[key]
+      if (typeof value === 'object') {
+        _scrub(value)
+      }
+    }
+  }
+  const output = Object.assign({}, meta)
+  _scrub(output)
+  return output
+}
+
 export function metadata({ bot, err, ...other }) {
   let data = {}
   if (bot && bot.config) {
@@ -17,7 +58,7 @@ export function metadata({ bot, err, ...other }) {
   return Object.assign({}, other, data)
 }
 
-export default new winston.Logger({
+const logger = new winston.Logger({
   transports: [
     new winston.transports.Console({
       level: config.winston.logger.console.level,
@@ -27,3 +68,8 @@ export default new winston.Logger({
     new RavenWinston({ dsn: config.sentry.dsn, patchGlobal: true, level: 'warn' }),
   ],
 })
+
+logger.rewriters.push(rewriteError)
+logger.rewriters.push(scrub)
+
+export default logger
