486f930dd4c06d2f6c324a3b79d5d2749ea75b27
Michael Hahn <mwhahn@gmail.com>
2016-06-09 01:51:04 -0700

    Prevent the user from editing themselves

--- client/components/ManageUsers/UserRow.js
+++ client/components/ManageUsers/UserRow.js
@@ -12,18 +12,30 @@ import ActionsRowColumn from 'c/ActionsRowColumn'
 
 import s from './user-row-style.scss'
 
-const UserRow = ({ user, onDelete, onEdit }) => {
+const UserRow = ({ user, onDelete, onEdit }, { viewer: { id: userId } }) => {
+  let actions = []
+  if (userId !== user.id) {
+    actions = [
+      <IconButton key='edit' onTouchTap={() => onEdit(user)}>
+        <FontIcon className='material-icons' color={colors.darkGrey}>edit</FontIcon>
+      </IconButton>,
+      <IconButton key='delete' onTouchTap={() => onDelete(user)}>
+        <FontIcon className='material-icons' color={colors.darkGrey}>delete</FontIcon>
+      </IconButton>
+    ]
+  }
+
+  let viewerLabel = ''
+  if (userId === user.id) {
+    viewerLabel = t(' (you)')
+  }
+
   return (
     <TableRow>
-      <TableRowColumn>{`@${user.username}`}</TableRowColumn>
+      <TableRowColumn>{`@${user.username}${viewerLabel}`}</TableRowColumn>
       <TableRowColumn>{user.displayRole}</TableRowColumn>
       <ActionsRowColumn>
-        <IconButton onTouchTap={() => onEdit(user)}>
-          <FontIcon className='material-icons' color={colors.darkGrey}>edit</FontIcon>
-        </IconButton>
-        <IconButton onTouchTap={() => onDelete(user)}>
-          <FontIcon className='material-icons' color={colors.darkGrey}>delete</FontIcon>
-        </IconButton>
+        {actions}
       </ActionsRowColumn>
     </TableRow>
   )
@@ -33,6 +45,10 @@ UserRow.propTypes = {
   user: PropTypes.object.isRequired,
   onDelete: PropTypes.func.isRequired,
   onEdit: PropTypes.func.isRequired,
+}
+
+UserRow.contextTypes = {
+  viewer: PropTypes.object.isRequired,
 }
 
 export default withStyles(s)(UserRow)
