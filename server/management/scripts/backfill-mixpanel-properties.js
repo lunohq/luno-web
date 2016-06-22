26eb9a7b41070a7d47ba0b7dd6efe93dc0da8cf2
Michael Hahn <mwhahn@gmail.com>
2016-06-22 16:24:58 -0700

    Add script to backfill mixpanel properties

---
+++ server/management/scripts/backfill-mixpanel-properties.js
@@ -0,0 +1,36 @@
+/**
+ * Script to backfill "email" and "role" within mixpanel.
+ *
+ */
+import Mixpanel from 'mixpanel'
+import { db } from 'luno-core'
+
+import logger from '../../logger'
+import config from '../../config/environment/index'
+import { sleep } from './helpers'
+
+export default async function() {
+  const mixpanel = Mixpanel.init(config.mixpanel.token)
+  const teams = await db.team.getTeams()
+  for (const team of teams) {
+    logger.info(`...updating users within team: ${team.id}`)
+    const users = await db.user.getUsers(team.id)
+    for (const index in users) {
+      const user = users[index]
+      const distinctId = `${team.id}:${user.id}`
+      const props = {
+        Role: user.role,
+      }
+      if (user.email) {
+        props.Email = user.email
+      } else if (user.profile && user.profile.email) {
+        props.Email = user.profile.email
+        logger.info(`! email not found for user`, { user })
+      }
+      logger.info(`...updating user props: ${distinctId}`, { props })
+      mixpanel.set(distinctId, props)
+    }
+    // Throttle updates so we don't exceed our provisioned throughput
+    await sleep(1)
+  }
+}
