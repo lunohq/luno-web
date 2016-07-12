f4a88640e70d065d78232afe13622c74639abcea
Michael Hahn <mwhahn@gmail.com>
2016-07-11 23:25:23 -0700

    Format values in the table

--- client/components/ThreadLogs/ThreadRow.js
+++ client/components/ThreadLogs/ThreadRow.js
@@ -4,14 +4,15 @@ import IconButton from 'material-ui/IconButton'
 import VisibilityIcon from 'material-ui/svg-icons/action/visibility'
 import { TableRow, TableRowColumn } from 'material-ui/Table'
 
+import moment from 'u/moment'
 import colors from 's/colors'
 
 import ActionsRowColumn from 'c/ActionsRowColumn'
 
 const ThreadRow = ({ thread, onViewMore }) => (
   <TableRow>
-    <TableRowColumn>{thread.created}</TableRowColumn>
-    <TableRowColumn>{thread.username}</TableRowColumn>
+    <TableRowColumn>{moment(thread.created).format('MMMM Do YYYY, h:mm:ss a')}</TableRowColumn>
+    <TableRowColumn>{`@${thread.username}`}</TableRowColumn>
     <TableRowColumn>{thread.channelName}</TableRowColumn>
     <TableRowColumn>{thread.message}</TableRowColumn>
     <ActionsRowColumn>
