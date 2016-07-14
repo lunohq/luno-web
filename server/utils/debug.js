1370b0390e4f4c572e521713d416248057b2932d
Michael Hahn <mwhahn@gmail.com>
2016-07-14 10:31:21 -0700

    Support debug when deployed

--- server/utils/debug.js
+++ server/utils/debug.js
@@ -2,6 +2,6 @@ import debug from 'debug'
 
 export default function (filename) {
   const parts = filename.split('/').map(part => part.split('.')[0])
-  const index = parts.indexOf('luno-web')
-  return debug(parts.slice(index + 1).join(':'))
+  const index = parts.indexOf('server')
+  return debug(parts.slice(index).join(':'))
 }
